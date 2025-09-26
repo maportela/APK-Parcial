547a074fddd4a02c017e91bd51efb7fcf83ff4d9
Maria Clara Martinez Bassi Portela <mariclaraportela@gmail.com>
2025-09-26 15:52:17 -0300

    Adicionando a primeira entrega do MediShare pro repositório

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonImg,
+  IonFooter,
+  IonButtons
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonImg,
+    IonFooter,
+    IonButtons
+  ],
 })
 export class HomePage {
-  constructor() {}
+  appName = 'MediShare';
+
+  apis = [
+    {
+      name: 'OpenFDA',
+      link: 'https://open.fda.gov/',
+      description: 'Informações sobre medicamentos'
+    },
+    {
+      name: 'RxNorm',
+      link: 'https://lhncbc.nlm.nih.gov/RxNav/APIs/index.html',
+      description: 'Base de dados de nomes de medicamentos'
+    },
+     {
+      name: 'ViaCEP',
+      link: 'https://viacep.com.br/',
+      description: 'Consulta endereços por CEP'
+    }
+  ];
+
+  nativeResource = 'Geolocalização (Google Maps) - Para encontrar os postos de saúde próximos à localização da pessoa';
+
+  students = [
+    {
+      name: 'Maria Clara Martinez Bassi Portela',
+      photo: 'assets/mariaclara.png'
+    },
+    {
+      name: 'Lucas Barros Mourão Silva',
+      photo: 'assets/lucasbarros.png'
+    }
+  ];
+
+  turma = 'ADS0301M';
+  turno = 'Manhã';
+  unidade = 'Bangu';
+  periodo = '4º semestre';
+  ano = '2025';
 }
